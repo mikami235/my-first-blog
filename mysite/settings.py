3c257d6ee0a3b97cbd8892ea197e088a0c1e3b20
三上 季範 <mikami235@gmail.com>
2020-04-08 22:59:38 +0900

    [chart_page.html, settings.py] csv file load func is tested

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print("BASE_DIR",BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,3 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+print("STATIC_ROOT",STATIC_ROOT)
